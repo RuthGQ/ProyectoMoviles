b466fae0f0734eb895ec09ea19fd4f7193519365
RuthGQ <rg02192004@gmail.com>
2023-06-11 16:22:14 -0500

    Se agrego menu

--- app/src/main/java/com/example/proyectomovil/entity/TarjetaEntity.kt
+++ app/src/main/java/com/example/proyectomovil/entity/TarjetaEntity.kt
@@ -1,4 +1,21 @@
 package com.example.proyectomovil.entity
 
-class TarjetaEntity {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tb_tarjeta")
+class TarjetaEntity (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name="idTarjeta")
+    val id:Int=0,
+
+    @ColumnInfo(name = "numeroTarj")
+    val numero:Int,
+
+    @ColumnInfo(name = "fechaTarj")
+    val fecha:String?,
+    ){
+
+
 }
--- app/src/main/java/com/example/proyectomovil/ui/view/fragment/PrincipalFragment.kt
+++ app/src/main/java/com/example/proyectomovil/ui/view/fragment/PrincipalFragment.kt
@@ -20,10 +20,6 @@ import dagger.hilt.android.AndroidEntryPoint
 //@AndroidEntryPoint
 class PrincipalFragment : Fragment(R.layout.fragment_principal) {
 
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var navigationView: NavigationView
-    private lateinit var toggle: ActionBarDrawerToggle
-
     private var _binding : FragmentPrincipalBinding? = null
     private val binding get() = _binding!!
 
@@ -38,98 +34,6 @@ class PrincipalFragment : Fragment(R.layout.fragment_principal) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    /*    drawerLayout = findViewById(R.id.drawer_layout)
-
-
-        setSupportActionBar(findViewById(R.id.toolbar))
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        navigationView = findViewById(R.id.nav_view)
-
-        toggle = ActionBarDrawerToggle(
-            this,
-            drawerLayout,
-            findViewById(R.id.toolbar),
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
-        )
-        drawerLayout.addDrawerListener(toggle)
-
-
-        val navigationView = findViewById<NavigationView>(R.id.nav_view)
-        navigationView.setNavigationItemSelectedListener { menuItem ->
-            // Handle menu item clicks here
-            when (menuItem.itemId) {
-                R.id.nav_home -> {
-                    // Handle home item click
-                    Toast.makeText(applicationContext, "Home clicked", Toast.LENGTH_SHORT).show()
-                    drawerLayout.closeDrawer(GravityCompat.START)
-                    return@setNavigationItemSelectedListener true
-
-                    true
-                }
-                R.id.nav_profile -> {
-                    // Handle profile item click
-                    Toast.makeText(applicationContext, "Profile clicked", Toast.LENGTH_SHORT).show()
-                    drawerLayout.closeDrawer(GravityCompat.START)
-                    true
-                }
-                R.id.nav_settings -> {
-                    // Handle settings item click
-                    Toast.makeText(applicationContext, "Settings clicked", Toast.LENGTH_SHORT).show()
-                    drawerLayout.closeDrawer(GravityCompat.START)
-                    drawerLayout.closeDrawers()
-                    true
-                }
-                R.id.nav_logout -> {
-                    // Handle logout item click
-                    Toast.makeText(applicationContext, "Logout clicked", Toast.LENGTH_SHORT).show()
-                    drawerLayout.closeDrawer(GravityCompat.START)
-                    true
-                }
-                R.id.nav_contactus -> {
-                    // Handle contact us item click
-                    Toast.makeText(applicationContext, "Contact us clicked", Toast.LENGTH_SHORT).show()
-                    drawerLayout.closeDrawer(GravityCompat.START)
-                    true
-                }
-                R.id.nav_rate -> {
-                    // Handle rate item click
-                    Toast.makeText(applicationContext, "Rate clicked", Toast.LENGTH_SHORT).show()
-                    drawerLayout.closeDrawer(GravityCompat.START)
-                    true
-                }
-                R.id.nav_about -> {
-                    // Handle about item click
-                    Toast.makeText(applicationContext, "About clicked", Toast.LENGTH_SHORT).show()
-                    drawerLayout.closeDrawer(GravityCompat.START)
-                    true
-                }
-                else -> false
-            }
-            true
-        }
-
-    }
-
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
-        toggle.syncState()
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        toggle.onConfigurationChanged(newConfig)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                drawerLayout.openDrawer(GravityCompat.START)
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }*/
     }
 
 
