83c95819f1e84765b9471b30c8d1a7782f790838
Sony <Sony>
2023-06-24 18:16:20 -0500

    [ADD] SE CREÓ FRAGMENT PARA REGISTRAR DIRECCION Y SE AGREGÓ REDIRECCIONAMIENTO A ESE FRAGMENT DESDE DireccionFragment

--- app/src/main/java/com/example/proyectomovil/ui/view/fragment/DireccionFragment.kt
+++ app/src/main/java/com/example/proyectomovil/ui/view/fragment/DireccionFragment.kt
@@ -5,16 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.proyectomovil.R
+import com.example.proyectomovil.databinding.FragmentDireccionBinding
+import com.example.proyectomovil.databinding.FragmentMenuBinding
 
-class DireccionFragment : Fragment() {
+class DireccionFragment : Fragment()  {
+
+    private var _binding : FragmentDireccionBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_direccion, container, false)
+        _binding = FragmentDireccionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnAgregar.setOnClickListener {
+            val action = DireccionFragmentDirections.actionDireccionFragmentToDireccionRegistrarFragment()
+            findNavController().navigate(action)
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/proyectomovil/ui/view/fragment/DireccionRegistrarFragment.kt
@@ -0,0 +1,6 @@
+package com.example.proyectomovil.ui.view.fragment
+
+import androidx.fragment.app.Fragment
+
+class DireccionRegistrarFragment : Fragment (){
+}
