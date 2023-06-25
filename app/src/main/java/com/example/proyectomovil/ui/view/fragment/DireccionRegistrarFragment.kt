274120d5ab060667daff45179265edd6726abcb6
Sony <Sony>
2023-06-24 20:41:26 -0500

    [ADD] SE AGREGO EL FRAGMENT REGISTRAR DIRECCION

--- app/src/main/java/com/example/proyectomovil/ui/view/fragment/DireccionRegistrarFragment.kt
+++ app/src/main/java/com/example/proyectomovil/ui/view/fragment/DireccionRegistrarFragment.kt
@@ -1,6 +1,24 @@
 package com.example.proyectomovil.ui.view.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.proyectomovil.databinding.FragmentDireccionRegistrarBinding
 
 class DireccionRegistrarFragment : Fragment (){
+
+    private var _binding : FragmentDireccionRegistrarBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDireccionRegistrarBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
 }
